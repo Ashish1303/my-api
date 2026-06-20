c78bd0a87fe126f157d831f13d148cf14a96b6f0
Ashish1303 <ashish130392@gmail.com>
2026-06-20 20:55:44 +0530

    db updated

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/myapi');
+    //mongodb+srv://ashish130392_db_user:JMBgXmnfQfzFl1Bs@mhcluster.5uhwklw.mongodb.net/
+
+    await mongoose.connect('mongodb://ashish130392_db_user:6K75gnxK2gcvZyNO@ac-n8swk9s-shard-00-00.qbogacx.mongodb.net:27017,ac-n8swk9s-shard-00-01.qbogacx.mongodb.net:27017,ac-n8swk9s-shard-00-02.qbogacx.mongodb.net:27017/?ssl=true&replicaSet=atlas-145byl-shard-0&authSource=admin&appName=MyClusterDB1');
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
